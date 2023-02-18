0de124e84bf63aa18cbd6bd510bc02027dd06cc6
pkstDev <William27060827@hotmail.com>
2023-02-18 22:11:10 +0800

    Initial (and hopefully last) commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shulker Peek
+
+The world's simplest shulker box preview using entirely one single [Mixin](src/main/java/io/github/prismwork/shulkerpeek/mixin/BlockItemMixin.java) and completely vanilla features.
+
+### What?
+
+In case you don't know, vanilla has a nice class, `BundleTooltipData`, to handle bundle inventory displaying. As you can see, this is not bad for shulker box inventory previewing! More importantly, it's super simple to adapt this to work with shulker boxes. See the code for more info.
---
+++ src/main/java/io/github/prismwork/shulkerpeek/mixin/BlockItemMixin.java
@@ -0,0 +1,34 @@
+package io.github.prismwork.shulkerpeek.mixin;
+
+import net.minecraft.client.item.BundleTooltipData;
+import net.minecraft.client.item.TooltipData;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.Optional;
+
+@Mixin(BlockItem.class)
+public abstract class BlockItemMixin extends Item {
+	public BlockItemMixin(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public Optional<TooltipData> getTooltipData(ItemStack stack) {
+		if (stack.isOf(Items.SHULKER_BOX)) {
+			NbtCompound nbt = BlockItem.getBlockEntityNbtFromStack(stack);
+			if (nbt != null) {
+				DefaultedList<ItemStack> inventory = DefaultedList.ofSize(27, ItemStack.EMPTY);
+				Inventories.readNbt(nbt, inventory);
+				return Optional.of(new BundleTooltipData(inventory, inventory.size()));
+			}
+		}
+		return super.getTooltipData(stack);
+	}
+}
